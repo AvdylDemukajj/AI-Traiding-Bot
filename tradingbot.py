05f3cf314f0a4521790cffafb72ea9419324a4fd
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-11 15:38:03 +0100

    callig configs in main file

--- tradingbot.py
+++ tradingbot.py
@@ -5,3 +5,9 @@ from lumibot.traders import Trader
 from datetime import datetime
 import config
 
+ALPACA_CREDS = {
+    "API_KEY": config.API_KEY,
+    "API_SECRET": config.API_SECRET,
+    "PAPER": True
+}
+
