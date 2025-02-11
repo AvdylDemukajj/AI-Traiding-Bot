394937f2ea7b9f6358bf58a0b9e3318359f02f99
AvdylDemukajj <avdyldemukaj38@gmail.com>
2025-02-11 15:36:59 +0100

    "importing libraries"

---
+++ tradingbot.py
@@ -0,0 +1,7 @@
+from lumibot.brokers import Alpaca
+from lumibot.backtesting import YahooDataBacktesting
+from lumibot.strategies.strategy import Strategy
+from lumibot.traders import Trader
+from datetime import datetime
+import config
+
